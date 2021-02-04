e1a2993364eb161670615eb248bbc4e4f2bdfa2f
harshwardhan sharma <hw.sharma9@gmail.com>
2021-02-04 21:54:07 +0530

    files updated

--- includes/placement2020.php
+++ includes/placement2020.php
@@ -1,45 +1,31 @@
 <div class="slider placement">
-    <h1>Placement 2020</h1>
+    <h1>Placement 2021</h1>
     <div id="placement-slider" class="owl-carousel owl-theme">
-        <div class="item">
-            <div class="pl-card">
-                <img loading="lazy" src="./images/facultyimg/" alt="Avatar" />
-                <div class="pl-card-container">
-                    <h4><b>John Doe</b></h4>
-                    <p>Company</p>
-                    <p>₹ 12000</p>
+        <?php 
+            $dir = './assets/placement2021';
+
+            if (is_dir($dir)){
+              if ($dh = opendir($dir)){
+                while (($file = readdir($dh)) !== false){ 
+                    if (!in_array($file, array('.', '..'))) {
+                        $exp = explode('.', basename($file));
+                        $name = explode('-', basename($exp[0]));
+                        $company = $name[1];
+            ?>
+                <div class="item">
+                    <div class="pl-card">
+                        <img loading="lazy" src="<?php echo $dir."/".$file; ?>" alt="Avatar" />
+                        <div class="pl-card-container">
+                            <h4><b><?php echo ucwords(str_replace("_", " ", $name[0])); ?></b></h4>
+                            <p><?php echo $company ?></p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="pl-card">
-                <img loading="lazy" src="./images/pl-slider/jonny.png" alt="Avatar" />
-                <div class="pl-card-container">
-                    <h4><b>John Doe</b></h4>
-                    <p>Company</p>
-                    <p>₹ 12000</p>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="pl-card">
-                <img loading="lazy" src="./images/pl-slider/jonny.png" alt="Avatar" />
-                <div class="pl-cardcontainer">
-                    <h4><b>John Doe</b></h4>
-                    <p>Company</p>
-                    <p>₹ 12000</p>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <div class="pl-card">
-                <img loading="lazy" src="./images/pl-slider/jonny.png" alt="Avatar" />
-                <div class="pl-card-container">
-                    <h4><b>John Doe</b></h4>
-                    <p>Company</p>
-                    <p>₹ 12000</p>
-                </div>
-            </div>
-        </div>
+            <?php }
+                }
+                closedir($dh);
+              }
+            }
+        ?>
     </div>
 </div>
