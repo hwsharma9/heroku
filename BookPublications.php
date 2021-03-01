0f0be0647d927c5ddd12c33a153ba08430c11183
lishu gupta <lishugupta652@gmail.com>
2021-03-01 23:48:42 +0530

    BOOKS PUBLICATION

--- BookPublications.php
+++ BookPublications.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="To achieve excellent standards of quality education by keeping pace with rapidly changing technologies & create computer professionals of global standards with capabilities of accepting new challenges.">
+    <meta name="description"
+        content="To achieve excellent standards of quality education by keeping pace with rapidly changing technologies & create computer professionals of global standards with capabilities of accepting new challenges.">
     <meta name="keywords" content="SCSIT, INDORE, DAVV, COMPUTER SCIENCE, COLLEGE">
     <meta name="author" content="scsit davv">
     <title>Scsit Davv</title>
@@ -14,6 +15,22 @@
     <?php include './includes/scriptincludes.php';?>
     <!-- SCRIPTS END -->
 </head>
+<style>
+.content-left img {
+    width: 100%;
+    min-width: 150px;
+    max-width: 150px;
+    min-height: 200px;
+    background-color: #454545;
+    max-height: 400px;
+    object-fit: cover;
+    position: relative;
+}
+
+.content-left h4 {
+    margin-left: 50px;
+}
+</style>
 
 <body>
 
@@ -33,43 +50,41 @@
                         <h1> Book Publications</h1>
                         <div class="books-items">
                             <div class="item" style="display:flex; flex-direction: row;">
-                                <div class="faculty-card">
-                                    <img loading="lazy" src="./images/BookPublications/SoftwareEngg..jpg" alt="Avatar"
-                                        style="width: 100%" />
+                                <div class="">
+                                    <img loading="lazy" src="./images/BookPublications/SoftwareEngg..jpg"
+                                        alt="Avatar" />
                                 </div>
-                                <div class="faculty-card-container" style="margin-top: 30px;">
+                                <div class="" style="margin: auto;">
                                     <h4><b>Dr.Ugrasen Suman, Software Engineering: Concepts & Practices, 1st
                                             Edition, Cengage Learning, 2013</b></h4>
                                 </div>
                             </div>
                             <div class="item" style="display:flex; flex-direction: row;">
-                                <div class="faculty-card">
-                                    <img loading="lazy" src="./images/BookPublications/Engg.jpg" alt="Avatar"
-                                        style="width: 100%" />
+                                <div class="">
+                                    <img loading="lazy" src="./images/BookPublications/Engg.jpg" alt="Avatar" />
                                 </div>
-                                <div class="faculty-card-container" style="margin-top: 30px;">
-                                    <h4><b>Dr.Ugrasen Suman, Software Engineering, 1st Edittion, Cengage Learning, 2018</b></h4>
-                                </div>
-                            </div>
-                            <div class="item" style="display:flex; flex-direction: row;">
-                                <div class="faculty-card">
-                                    <img loading="lazy" src="./images/BookPublications/OOPs.jpg" alt="Avatar"
-                                        style="width: 100%" />
-                                </div>
-                                <div class="faculty-card-container" style="margin-top: 30px;">
-                                    <h4><b>Dr.Ugrasen Suman, Dr. Sanjeev Kumar Sharma, Dr. Maya Rathore, Object
-                                            Oriented Analysis & Design using UML, 1st Edition, Cengage Learning,
+                                <div class="" style="margin: auto;">
+                                    <h4><b>Dr.Ugrasen Suman, Software Engineering, 1st Edittion, Cengage Learning,
                                             2018</b></h4>
                                 </div>
                             </div>
                             <div class="item" style="display:flex; flex-direction: row;">
-                                <div class="faculty-card">
-                                    <img loading="lazy" src="./images/BookPublications/BigData.jpg" alt="Avatar"
-                                        style="width: 100%" />
+                                <div class="">
+                                    <img loading="lazy" src="./images/BookPublications/BigData.jpg" alt="Avatar" />
                                 </div>
-                                <div class="faculty-card-container" style="margin-top: 30px;">
+                                <div class="" style="margin: auto;">
                                     <h4><b>Dr.Rajkamal, Dr.Preeti Saxena, Big Data Analytics, Tata McGraw Hill,
                                             2019</b></h4>
+                                </div>
+                            </div>
+                            <div class="item" style="display:flex; flex-direction: row;">
+                                <div class="">
+                                    <img loading="lazy" src="./images/BookPublications/OOPs.jpg" alt="Avatar" />
+                                </div>
+                                <div class="" style="margin: auto;">
+                                    <h4><b>Dr.Ugrasen Suman, Dr. Sanjeev Kumar Sharma, Dr. Maya Rathore, Object
+                                            Oriented Analysis & Design using UML, 1st Edition, Cengage Learning,
+                                            2018</b></h4>
                                 </div>
                             </div>
                         </div>
