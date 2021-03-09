ca6024efca2738d44ec8649d4044758824b432d7
harshwardhan sharma <hw.sharma9@gmail.com>
2021-03-09 21:33:25 +0530

    sitemap file updated

--- sitemap.php
+++ sitemap.php
@@ -38,72 +38,105 @@
                     <!-- ADD NEW CONTENT DOWN HERE -->
                     <h1>Sitemap</h1>
                     <ul>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
-                        <a href="#">
-                            <li class="content-link">Research</li>
-                        </a>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/" title="Scsit Davv">Scsit Davv</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/index.php" title="Scsit Davv">index page</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/about.php" title="Scsit Davv">about us</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/infrastructure.php" title="Scsit Davv">infrastructure</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/coursesoffered.php" title="Scsit Davv">courses offered</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/fees.php" title="Scsit Davv">fees</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/howtoapply.php" title="Scsit Davv">how to apply</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/coursescheme.php" title="Scsit Davv">course scheme</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/assessment.php" title="Scsit Davv">assessment</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/academic_planning_committee.php" title="Scsit Davv">academic planning committee</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/EducationwithICT.php" title="Scsit Davv">Education with ICT</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/research.php" title="Scsit Davv">research</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/ResearchCenter.php" title="Scsit Davv">Research Center</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/ResearchFellowship.php" title="Scsit Davv">Research Fellowship</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/faculty.php" title="Scsit Davv">faculty</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/staff.php" title="Scsit Davv">staff</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/student.php" title="Scsit Davv">student</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/why_recruit_us.php" title="Scsit Davv">why recruit us</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/placement.php" title="Scsit Davv">placement</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/placement_committee.php" title="Scsit Davv">placement committee</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/career_counselling.php" title="Scsit Davv">career counselling</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/consultancy.php" title="Scsit Davv">consultancy</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/syllabus.php" title="Scsit Davv">syllabus</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Lab_Manual.php" title="Scsit Davv">Lab Manual</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/counsellor.php" title="Scsit Davv">counsellor</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/scholarship.php" title="Scsit Davv">scholarship</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/onlineskilltest.php" title="Scsit Davv">online skill test</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/studentlife.php" title="Scsit Davv">student life</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/head.php" title="Scsit Davv">head</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/news.php" title="Scsit Davv">news</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/exam&results.php" title="Scsit Davv">exam</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/library.php" title="Scsit Davv">library</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/MOU's&Projects.php" title="Scsit Davv">MOU</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/awards_and_achievements.php" title="Scsit Davv">awards and achievements</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/ex_student_verification.php" title="Scsit Davv">ex student verification</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/upcoming_events.php" title="Scsit Davv">upcoming events</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/campustour.php" title="Scsit Davv">campustour</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/photo_gallery.php" title="Scsit Davv">photo gallery</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/downloads.php" title="Scsit Davv">downloads</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/sitemap.php" title="Scsit Davv">sitemap</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/reachus.php" title="Scsit Davv">reachus</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/developer.php" title="Developer's page">Developer's page</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/vision.php" title="Scsit Davv">vision</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Labs_Embaded.php" title="Scsit Davv">Labs Embaded</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Labs_Database.php" title="Scsit Davv">Labs Database</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Labs_hci.php" title="Scsit Davv">Labs hci</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Labs_vlsi.php" title="Scsit Davv">Labs vlsi</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Labs_ecommerce.php" title="Scsit Davv">Labs ecommerce</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Why_Assessment.php" title="Scsit Davv">Why Assessment</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Assessment_Methods.php" title="Scsit Davv">Assessment Methods</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Improvement_Process.php" title="Scsit Davv">Improvement Process</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Impact_of_Assessment.php" title="Scsit Davv">Impact of Assessment</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Assessment_Research_Committee.php" title="Scsit Davv">Assessment Research Committee</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/IQAC.php" title="Scsit Davv">IQAC</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/DepartmentalResearchCommittee.php" title="Scsit Davv">Departmental Research Committee</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/ResearchSupervisors.php" title="Scsit Davv">Research Supervisors</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Facilities.php" title="Scsit Davv">Facilities</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Publications.php" title="Scsit Davv">Publications</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/CodeEthicsResearch.php" title="Scsit Davv">Code Ethics Research</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/s_tanwani.php" title="Scsit Davv">sanjay tanwani</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/u_suman.php" title="Scsit Davv">ugrasen suman</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/p_saxsena.php" title="Scsit Davv">preeti saxsena</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/s_masih.php" title="Scsit Davv">shraddha masih</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Ajay_Tiwari.php" title="Scsit Davv">Ajay Tiwari</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Archana_Chaudhary.php" title="Scsit Davv">Archana Chaudhary</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Deepak_Abhyankar.php" title="Scsit Davv">Deepak Abhyankar</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Nitin_Uikey.php" title="Scsit Davv">Nitin Uikey</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Chaitali_Uikey.php" title="Scsit Davv">Chaitali Uikey</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Hitesh_Ninama.php" title="Scsit Davv">Hitesh Ninama</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Chetan_Awasthi.php" title="Scsit Davv">Chetan Awasthi</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Pankaj_Jagtap.php" title="Scsit Davv">Pankaj Jagtap</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Mohit_Kumar_Varma.php" title="Scsit Davv">Mohit Kumar Varma</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Tarjani_Sevak.php" title="Scsit Davv">Tarjani Sevak</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Deepika_Rai.php" title="Scsit Davv">Deepika Rai</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Sunita_Gaur.php" title="Scsit Davv">Sunita Gaur</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/K_L_Suryawanshi.php" title="Scsit Davv">KL Suryawanshi</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/HimanshukamalVerma.php" title="Scsit Davv">Himanshu kamal Verma</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/m_ingle.php" title="Scsit Davv">maya ingle</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Priyesh_Kanungo.php" title="Scsit Davv">Priyesh Kanungo</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Dr.AlokTiwari.php" title="Scsit Davv">Dr AlokTiwari</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/a_more.php" title="Scsit Davv">anand more</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/abilityprofiler.php" title="Scsit Davv">ability profiler</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Previous_Student_kit.php" title="Scsit Davv">Previous Student kit</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/wificampus.php" title="Scsit Davv">wifi campus</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/nss.php" title="Scsit Davv">nss</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/learnbyearnscheme.php" title="Scsit Davv">learn by earn scheme</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/remedialclasses.php" title="Scsit Davv">remedial classes</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/languagelab.php" title="Scsit Davv">language lab</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/swachhbharatabhiyan.php" title="Scsit Davv">swachh bharat abhiyan</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/samarpan.php" title="Scsit Davv">samarpan</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/bloodbonation.php" title="Scsit Davv">blood donation</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/treeplantation.php" title="Scsit Davv">tree plantation</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/sports.php" title="Scsit Davv">sports</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/examination_committee.php" title="Scsit Davv">examination committee</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/Time_Table.php" title="Scsit Davv">Time Table</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/BookPublications.php" title="Scsit Davv">Book Publications</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/BookChapterPublications.php" title="Scsit Davv">Book Chapter Publications</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/JournalPublications.php" title="Scsit Davv">Journal Publications</a></li>
+                        <li class="content-link"><a href="http://harshherokuapp.herokuapp.com/ConferencePublications.php" title="Scsit Davv">Conference Publications</a></li>
                     </ul>
                 </div>
 
