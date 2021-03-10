d5aedc0781d95119333535e777708669bc74b117
lishu gupta <lishugupta652@gmail.com>
2021-03-10 18:38:51 +0530

    usuman under supervision

--- Usuman_under_supervision.php
+++ Usuman_under_supervision.php
@@ -28,56 +28,67 @@
         <div class="page-title"></div>
 
         <!-- main content area -->
-       
+
         <div class="primary-container">
             <div class="main-content-area" id="main-content-area">
                 <div class="content-left padding-border">
                     <!-- ADD NEW CONTENT DOWN HERE -->
                     <h1>Dr. Ugrasen Suman</h1>
-                    <h2>  PhD Thesis Supervised</h2>
+                    <h2> PhD Thesis Supervised</h2>
                     <hr>
                     <p>
-                
-                            <li>Towards designing an effective software process for global software development
-                                (Ritu Jain, 2019).
-                            </li>
-                            <li> Implementation of dynamic web services orchestration for service oriented architecture
-                                 (Reena Gupta, 2018).
-                                </li>
-                            <li>Investigation of cloud adoption issues and designing a secure cloud computing environment (Nitin Nagar, 2018)</li>
-                             <li>Limitations and solutions for agile software development methodologies (Nitin Uikey, 2017)</li>
-                    <li>Performance analysis of QoS broker based architectural model for dynamic web service discovery and composition (Maya Rathore, 2016)</li>
-                    <li>A framework for cryptographically secured framework web service composition (Rajat Bhandari, 2015)</li>
-                    <li>Investigation of extreme programming practices in software maintenance (Jitender Chaudhari, 2014)</li>
-                    <li>Design and development of intelligent web mining system for extraction of information from Web-databases (Sanjeev Kumar Sharma, 2013)</li>
-                    <li>Investigation of the effectiveness of requirement engineering practices in software development process (Dhirendra Pandey, 2012)</li>
-                    
+
+                    <ul>
+
+                        <li>Towards designing an effective software process for global software development
+                            (Ritu Jain, 2019).
+                        </li>
+                        <li> Implementation of dynamic web services orchestration for service oriented architecture
+                            (Reena Gupta, 2018).
+                        </li>
+                        <li>Investigation of cloud adoption issues and designing a secure cloud computing environment
+                            (Nitin Nagar, 2018)</li>
+                        <li>Limitations and solutions for agile software development methodologies (Nitin Uikey, 2017)
+                        </li>
+                        <li>Performance analysis of QoS broker based architectural model for dynamic web service
+                            discovery and composition (Maya Rathore, 2016)</li>
+                        <li>A framework for cryptographically secured framework web service composition (Rajat Bhandari,
+                            2015)</li>
+                        <li>Investigation of extreme programming practices in software maintenance (Jitender Chaudhari,
+                            2014)</li>
+                        <li>Design and development of intelligent web mining system for extraction of information from
+                            Web-databases (Sanjeev Kumar Sharma, 2013)</li>
+                        <li>Investigation of the effectiveness of requirement engineering practices in software
+                            development process (Dhirendra Pandey, 2012)</li>
+
                         </p>
-                    <h2>M.Tech (CS) by Research Supervised</h2>
-                    <hr>
-                    
-                        
-                            <li> A Design Model Incorporating Extended UML for Improvisation in Aspect-Oriented Design (Kirti Muley, 2012)</li>
-                            <li>Design Anti-patterns in Service Oriented Architecture (Deepali Tripathi, 2012)</li>
-                            <li>Performance evaluation of Web-enabled Multitier Models (Sanjiv Kumar, 2012)</li>
-                            <li>Translation facilities on mobile from English to Hindi (Shashi Pal Singh, 2012)</li>
-                    
-                    <h2>M.Tech & Other PG Projects Supervised</h2>
-                    <hr />
-                
-                    
-                            <li> 60 M.Tech Projects</li>
-                            <li>More than 100 MCA/ M.Sc. Projects</li>
+                        <h2>M.Tech (CS) by Research Supervised</h2>
+                        <hr>
 
-                    
-                    
-                    
 
+                        <li> A Design Model Incorporating Extended UML for Improvisation in Aspect-Oriented Design
+                            (Kirti Muley, 2012)</li>
+                        <li>Design Anti-patterns in Service Oriented Architecture (Deepali Tripathi, 2012)</li>
+                        <li>Performance evaluation of Web-enabled Multitier Models (Sanjiv Kumar, 2012)</li>
+                        <li>Translation facilities on mobile from English to Hindi (Shashi Pal Singh, 2012)</li>
+
+                        <h2>M.Tech & Other PG Projects Supervised</h2>
+                        <hr />
+
+
+                        <li> 60 M.Tech Projects</li>
+                        <li>More than 100 MCA/ M.Sc. Projects</li>
+
+
+
+
+
+                    </ul>
                 </div>
 
                 <div class="sidebar content-right">
                     <div class="sidebar-content" id="sidebar">
-                        
+
 
                         <!-- SIDEBAR LINKS START   -->
                         <?php include './includes/sidebarlinks.php';?>
